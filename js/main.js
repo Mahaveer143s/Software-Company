cc951125aeb298da1ba8aa85acfb1e1361bf19d7
Mahaveer143s <mahaveer621s@gmail.com>
2024-10-25 16:01:07 +0530

    Index page 50% completed

--- js/main.js
+++ js/main.js
@@ -63,3 +63,30 @@
     
 })(jQuery);
 
+const counters = document.querySelectorAll('.custom-count');
+  
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 25);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        updateCount();
+        observer.unobserve(counter);
+      }
+    });
+  }, { threshold: 1 });
+
+  observer.observe(counter);
+});
